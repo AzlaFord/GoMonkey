7570557f9f20f13d69e106b67408e04e95f64e11
bivol <vbivol394@gmail.com>
2026-03-25 13:50:20 +0200

    terminat de implementat test lexer pentru input =+(){},;

---
+++ lexer/lexer.go
@@ -0,0 +1,58 @@
+package lexer
+
+import "monkey/token"
+
+type Lexer struct {
+	input        string
+	position     int  // pozitia currenta in input  duce la caracterul curet
+	readPosition int  // pozitia currenta de citire din input dupa caracterul current
+	ch           byte // caracterul curent in evaluare care pozitia la varaibila position
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) { // verifica daca a ajuns la sfarsitul liniei
+		l.ch = 0 // e ASCII cod pentru null valaore 0 si semnifica ori nu am citit nimic inca ori e sfarsit de file EOF
+	} else {
+		l.ch = l.input[l.readPosition] // daca nu am ajuns la farsitul input-ului seteaza l.ch ca urmatorul caracter l.ch =l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+	l.readChar()
+	return tok
+
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
