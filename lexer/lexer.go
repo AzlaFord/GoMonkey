8cc262b072991726e74ce3d489cdf9e48a3ad0f2
bivol <vbivol394@gmail.com>
2026-03-26 10:18:56 +0200

    cresc testul lexer si creat fuctie de intentificat daca e o litera

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -47,12 +47,32 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 	l.readChar()
 	return tok
 
 }
 
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+	// in go poti compara caractere din cauza ca se ia in cosiderearea valaorea lor ASCII deci
+	// daca as compara 'R' == "R" as compara defata daca  52 == 52
 }
