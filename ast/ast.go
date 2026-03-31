41730a0350f885900885748fd6937017badf2296
bivol <vbivol394@gmail.com>
2026-03-31 11:50:23 +0300

    adugat Indetificator si populat campulrile token.Token

--- ast/ast.go
+++ ast/ast.go
@@ -1,7 +1,15 @@
 package ast
 
+import "monkey/token"
+
 type Node interface {
 	TokenLiteral() string
+}
+
+type LetStatement struct {
+	Token token.Token // token cu tip = let token.LET
+	Name  *Identifier // variabila ex: x
+	Value Expression  // valaorea ex : 5
 }
 
 type Statment interface {
@@ -15,6 +23,20 @@ type Expression interface {
 
 type Program struct {
 	Statments []Statment
+}
+
+type Identifier struct { // indetificatorul reprezinta x
+	Token token.Token // e token.IDENT
+	Value string      // contine valaorea lui x
+}
+
+func (ls *LetStatement) StatmentNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
 }
 
 func (p *Program) TokenLiteral() string {
