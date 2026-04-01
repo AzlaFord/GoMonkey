5dc4e10f4948591676cc2ebfdb2d199f1f7c0d24
bivol <vbivol394@gmail.com>
2026-04-01 12:08:48 +0300

    ParseProgram am facut

--- parser/parser.go
+++ parser/parser.go
@@ -27,3 +27,17 @@ func (p *Parser) nextToken() {
 func (p *Parser) ParseProgram() *ast.Program {
 	return nil
 }
+
+func (p *Parser) ParseProgram() *ast.Program {
+
+	program := p.parseStatement()
+	program.Statment = []ast.Statment{}
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
