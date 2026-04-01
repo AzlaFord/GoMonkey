b9d1d1d944fa3eeabb7dd14b8a68afa3677fcda6
bivol <vbivol394@gmail.com>
2026-04-01 12:37:08 +0300

    creat verificare la tokenuri si testul functioneaza

--- parser/parser.go
+++ parser/parser.go
@@ -24,20 +24,62 @@ func (p *Parser) nextToken() {
 	p.curToken = p.peekToken // se ia valaorea urmatoare si o pune in cur token
 	p.peekToken = p.l.NextToken()
 }
-func (p *Parser) ParseProgram() *ast.Program {
-	return nil
-}
 
 func (p *Parser) ParseProgram() *ast.Program {
-
-	program := p.parseStatement()
-	program.Statment = []ast.Statment{}
+	program := &ast.Program{}
+	program.Statments = []ast.Statment{}
 	for p.curToken.Type != token.EOF {
 		stmt := p.parseStatement()
 		if stmt != nil {
-			program.Statements = append(program.Statements, stmt)
+			program.Statments = append(program.Statments, stmt)
 		}
 		p.nextToken()
 	}
 	return program
+}
+
+func (p *Parser) parseStatement() ast.Statment {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectedPeek(token.IDENT) {
+		// daca nu e numele varaibile gasit ca urmatorul token se termina
+		return nil
+	}
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectedPeek(token.ASSIGN) {
+		// daca nu e gasit semnul = retunreaza errore
+		return nil
+	}
+
+	if !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectedPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
 }
